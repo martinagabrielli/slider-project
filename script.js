19f3fa5cb9f158c74e32663f7b8a2970d216b86b
Martina Gabrielli <martina.gabrielli@yahoo.it>
2020-02-08 18:02:59 +0000

    buttons slide functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+const pictures = ['cat1', 'cat2', 'cat3', 'cat4', 'cat5'];
+
+let imgContainer = document.getElementById('img-container');
+let buttonRight = document.getElementsByClassName('btn-right')[0];
+let buttonLeft = document.getElementsByClassName('btn-left')[0];
+let counter = 0;
+
+imgContainer.style.backgroundImage = `url('./img/${pictures[0]}.jpg')`;
+
+function slideRight() {
+  counter++;
+  if(counter > pictures.length -1){
+    counter = 0;
+  }
+  imgContainer.style.backgroundImage = `url('./img/${pictures[counter]}.jpg')`;
+}
+
+function slideLeft() {
+  counter--;
+  if(counter < 0){
+    counter = pictures.length -1;
+  }
+  imgContainer.style.backgroundImage = `url('./img/${pictures[counter]}.jpg')`;
+}
+
+buttonRight.addEventListener('click', slideRight);
+buttonLeft.addEventListener('click', slideLeft);
